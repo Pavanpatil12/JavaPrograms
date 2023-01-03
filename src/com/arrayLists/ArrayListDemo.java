484436381b515bdf54b86a44ab9ad1a20bad7384
Patilpavan12 <patilpavan191999@gmail.com>
2023-01-03 11:41:35 +0530

    hiii

---
+++ src/com/arrayLists/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.arrayLists;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo{
+	public static void main(String[]args) {
+		ArrayList arraylist = new ArrayList();
+		arraylist.add(10);
+		arraylist.add(20);
+		arraylist.add(30);
+		arraylist.add(40);
+		System.out.println(arraylist);
+
+	}
+	
+
+}
